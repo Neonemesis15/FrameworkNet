6244e13a0539131fe938b0c82279a3e5b81423ea
Neonemesis15 <pablo.salas.alvarez88@gmail.com>
2018-06-22 10:32:21 -0500

    add example interface

---
+++ Data/Common/AppMobile/DaoPersona.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lucky.Data.Common.AppMobile
+{
+    interface DaoPersona
+    {
+        public List<Object[]> personaQry();
+        public List<Object[]> personaQry(Int16 idPersona);
+    }
+}
---
+++ Data/Common/AppMobile/DaoPersonaImpl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lucky.Data.Common.AppMobile
+{
+    class DaoPersonaImpl : DaoPersona
+    {
+        public List<object[]> personaQry()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<object[]> personaQry(short idPersona)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
